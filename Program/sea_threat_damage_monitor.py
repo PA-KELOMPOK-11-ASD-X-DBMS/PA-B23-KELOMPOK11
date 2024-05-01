439a0ebc4b7e91d0cdc715d7ff94ed77763c78cb
AhmadNuralfansyah <144907524+AhmadNuralfansyah@users.noreply.github.com>
2024-05-01 18:27:09 +0800

    tambah keyboard interupt sea_threat_damage_monitor.py

--- Program/sea_threat_damage_monitor.py
+++ Program/sea_threat_damage_monitor.py
@@ -14,6 +14,15 @@ def buat_koneksi():
         database="sea_threat_damage_monitor",  
         autocommit=True  
     )
+    
+def get_input(prompt):
+    while True:
+        try:
+            user_input = input(prompt)
+            # Lakukan validasi atau pemrosesan tambahan di sini jika diperlukan
+            return user_input
+        except KeyboardInterrupt:
+            print("\nKeyboardInterrupt terdeteksi. Masukkan dibatalkan.")
 
 class User:  
     def __init__(self, nama_lengkap, no_hp,):  
@@ -84,7 +93,7 @@ class Admin(User):
             cursor.close()  
 
     def menu_sorting_data_kerusakan(self):  
-        choice = input("\nIngin Sorting Berdasarkan Apa? (Lokasi/Tanggal): ").lower()  
+        choice = get_input("\nIngin Sorting Berdasarkan Apa? (Lokasi/Tanggal): ").lower()  
         conn = buat_koneksi()  
         if choice == "lokasi":  
             self.sort_merge_data_kerusakan(conn, key=1)  
@@ -119,7 +128,7 @@ class Admin(User):
             conn.close()  
 
     def menu_sorting_aduan(self):  
-        choice = input("\nIngin Sorting Berdasarkan Apa? (Lokasi/Tanggal): ").lower()  
+        choice = get_input("\nIngin Sorting Berdasarkan Apa? (Lokasi/Tanggal): ").lower()  
         conn = buat_koneksi()  
         if  choice == "lokasi":   
             self.sort_merge_aduan(conn, key=2)  
@@ -383,7 +392,7 @@ class Admin(User):
                 print("3. Limbah laut")
                 print("4. Kapal laut")
                 print("5. Lainnya")
-                jenis_kerusakan_input = input("Silahkan Pilih jenis kerusakan: ") 
+                jenis_kerusakan_input = get_input("Silahkan Pilih jenis kerusakan: ") 
                 if jenis_kerusakan_input == "1":  
                     jenis_kerusakan = "Hewan laut" 
                 elif jenis_kerusakan_input == "2":
@@ -398,10 +407,10 @@ class Admin(User):
                     print("Pilihan kategori tidak valid.")
 
                 if jenis_kerusakan_input in {"1", "2", "3", "4", "5"}:  
-                    deskripsi_kerusakan = input("Perbarui Keterangan: ")  
+                    deskripsi_kerusakan = get_input("Perbarui Keterangan: ")  
 
                     while True:  
-                        jumlah_kerusakan = input("Masukan Jumlah Kerusakan: ")  
+                        jumlah_kerusakan = get_input("Masukan Jumlah Kerusakan: ")  
                         if jumlah_kerusakan.isdigit():   
                             jumlah_kerusakan = int(jumlah_kerusakan)   
                             break
@@ -440,7 +449,7 @@ class Admin(User):
             table.max_width["Keterangan"] = 80  
             print(table)
 
-            id_aduan = input("\nPilih ID_aduan aduan --> data kerusakan(kosongkan untuk kembali): ")
+            id_aduan = get_input("\nPilih ID_aduan aduan --> data kerusakan(kosongkan untuk kembali): ")
             if id_aduan:
                 result = self.pindahkan_ke_data_kerusakan(cursor, id_aduan, self.ID_Admin)
 
@@ -550,10 +559,10 @@ class Masyarakat(User):
 
     def edit_informasi_akun(self):   
         print("\nEdit Informasi Akun:")
-        alamat_baru = input("Masukkan alamat baru: ")  
+        alamat_baru = get_input("Masukkan alamat baru: ")  
         
         while True:  
-            nomor_hp_baru = input("Masukkan nomor HP baru: ")  
+            nomor_hp_baru = get_input("Masukkan nomor HP baru: ")  
             if nomor_hp_baru.isdigit():  
                 break
             else:
@@ -584,7 +593,7 @@ class Masyarakat(User):
             print("\n\033[0m\033[91m1.\033[0m Edit Informasi Akun")
             print("\033[0m\033[91m2.\033[0m Kembali ke Menu Utama")
 
-            pilihan = input("Masukkan pilihan Anda: ")
+            pilihan = get_input("Masukkan pilihan Anda: ")
 
             if pilihan == "1":
                 self.edit_informasi_akun() 
@@ -652,7 +661,7 @@ class Masyarakat(User):
             cursor.close()  
 
     def menu_sorting_data_kerusakan(self):  
-        choice = input("\nIngin Sorting Berdasarkan Apa? (Lokasi/Tanggal): ").lower()  
+        choice = get_input("\nIngin Sorting Berdasarkan Apa? (Lokasi/Tanggal): ").lower()  
         conn = buat_koneksi()  
         if choice == "lokasi":  
             self.sort_merge_data_kerusakan(conn, key=1)   
@@ -878,7 +887,7 @@ def menu_admin(admin):
         print("\033[0m\033[91m3.\033[0m Mengurus Akun Masyarakat") 
         print("\033[0m\033[91m4.\033[0m Keluar")
 
-        pilihan = input("Silahkan Pilih Menu(1/2/3/4): ")
+        pilihan = get_input("Silahkan Pilih Menu(1/2/3/4): ")
 
         if pilihan == "1":
             while True:
@@ -888,26 +897,26 @@ def menu_admin(admin):
                 print("\033[0m\033[91m3.\033[0m Update Table Data Kerusakan") 
                 print("\033[0m\033[91m4.\033[0m Kembali Ke Menu Admin")
 
-                sub_pilihan = input("Silahkan Pilih Menu(1/2/3/4): ")
+                sub_pilihan = get_input("Silahkan Pilih Menu(1/2/3/4): ")
 
                 if sub_pilihan == "1":
                     admin.lihat_data_kerusakan()  
-                    choice = input("\nIngin Sorting atau Searching Data Kerusakan? (Sort/Search): ").lower()
+                    choice = get_input("\nIngin Sorting atau Searching Data Kerusakan? (Sort/Search): ").lower()
                     if choice == "sort":
                         admin.menu_sorting_data_kerusakan()  
                     elif choice == "search":
-                        sub_choice = input("Ingin mencari berdasarkan ID atau Lokasi? (ID/Lokasi/Tanggal): ").lower()
+                        sub_choice = get_input("Ingin mencari berdasarkan ID atau Lokasi? (ID/Lokasi/Tanggal): ").lower()
                         if sub_choice == "id":
-                            id_data = input("Masukkan ID data yang ingin dicari: ")
+                            id_data = get_input("Masukkan ID data yang ingin dicari: ")
                             if id_data.isdigit():  
                                 admin.menu_search_data_kerusakan("ID_data", id_data)  
                             else:
                                 print("ID harus berupa angka. Silakan coba lagi.")
                         elif sub_choice == "lokasi":
-                            lokasi = input("Masukkan nama lokasi yang ingin dicari: ")
+                            lokasi = get_input("Masukkan nama lokasi yang ingin dicari: ")
                             admin.menu_search_data_kerusakan("lokasi", lokasi)  
                         elif sub_choice == "tanggal":
-                            tanggal = input("Masukkan tanggal yang ingin dicari (Format: DD-MM-YYYY): ")
+                            tanggal = get_input("Masukkan tanggal yang ingin dicari (Format: DD-MM-YYYY): ")
                             admin.menu_search_data_kerusakan("tanggal", tanggal)  
                         else:
                             print("Pilihan tidak valid. Silakan coba lagi.")
@@ -915,7 +924,7 @@ def menu_admin(admin):
                         print("Pilihan tidak ada!")
                 elif sub_pilihan == "2":
                     admin.lihat_data_kerusakan()  
-                    id_data = input("Masukkan ID data yang ingin dihapus: ")
+                    id_data = get_input("Masukkan ID data yang ingin dihapus: ")
                     if id_data.isdigit():  
                         admin.hapus_data_kerusakan(id_data)   
                     else:
@@ -936,26 +945,26 @@ def menu_admin(admin):
                 print("\033[0m\033[91m2.\033[0m Hapus Aduan Masyarakat")
                 print("\033[0m\033[91m3.\033[0m Kembali Ke Menu Admin")
 
-                sub_pilihan = input("Silahkan Pilih Menu(1/2/3): ")
+                sub_pilihan = get_input("Silahkan Pilih Menu(1/2/3): ")
 
                 if sub_pilihan == "1":
                     admin.lihat_aduan_masyarakat()  
-                    choice = input("\nIngin Sorting atau Searching Data Aduan? (Sort/Search): ").lower()
+                    choice = get_input("\nIngin Sorting atau Searching Data Aduan? (Sort/Search): ").lower()
                     if choice == "sort":
                         admin.menu_sorting_aduan()   
                     elif choice == "search":
-                        sub_choice = input("Ingin mencari berdasarkan ID atau Lokasi? (ID/Lokasi/Tanggal): ").lower()
+                        sub_choice = get_input("Ingin mencari berdasarkan ID atau Lokasi? (ID/Lokasi/Tanggal): ").lower()
                         if sub_choice == "id":
-                            id_data = input("Masukkan ID data yang ingin dicari: ")
+                            id_data = get_input("Masukkan ID data yang ingin dicari: ")
                             if id_data.isdigit():  
                                 admin.menu_search_aduan("ID Aduan", id_data)  
                             else:
                                 print("ID Aduan tidak ditemukan. ID harus berupa angka!")
                         elif sub_choice == "lokasi":
-                            lokasi = input("Masukkan nama lokasi yang ingin dicari: ")
+                            lokasi = get_input("Masukkan nama lokasi yang ingin dicari: ")
                             admin. menu_search_aduan("lokasi", lokasi)  
                         elif sub_choice == "tanggal":
-                            tanggal = input("Masukkan tanggal yang ingin dicari (Format: YYYY-MM-DD): ")
+                            tanggal = get_input("Masukkan tanggal yang ingin dicari (Format: YYYY-MM-DD): ")
                             admin.menu_search_aduan("tanggal", tanggal)  
                         else:
                             print("Pilihan tidak valid. Silakan coba lagi.")
@@ -963,7 +972,7 @@ def menu_admin(admin):
                         print("Pilihan tidak ada!")
                 elif sub_pilihan == "2":
                     admin.lihat_aduan_masyarakat()  
-                    id_aduan = input("Masukkan ID aduan yang ingin dihapus: ")
+                    id_aduan = get_input("Masukkan ID aduan yang ingin dihapus: ")
                     admin.hapus_aduan(id_aduan)  
                 elif sub_pilihan == "3":
                     break
@@ -973,9 +982,9 @@ def menu_admin(admin):
         elif pilihan == "3":
             admin.lihat_akun_masyarakat()  
             while True:
-                choice = input("\nIngin Searching Akun Masyarakat? (Y/N): ").lower()
+                choice = get_input("\nIngin Searching Akun Masyarakat? (Y/N): ").lower()
                 if choice == "y":
-                    key = input("Masukkan ID Masyarakat: ")
+                    key = get_input("Masukkan ID Masyarakat: ")
                     if not key.isdigit():  
                         print("ID Masyarakat harus berupa angka.")
                         continue
@@ -1001,14 +1010,14 @@ def buat_akun_masyarakat():
     conn = buat_koneksi()  
     cursor = conn.cursor()  
     try:  
-        ID_Masyarakat = input("Masukkan No KTP Anda:")
+        ID_Masyarakat = get_input("Masukkan No KTP Anda:")
         if not ID_Masyarakat.isdigit():  
             raise ValueError("No KTP harus berupa angka.")
         
-        nama_lengkap = input("Masukkan nama lengkap Anda: ")
-        alamat_rumah = input("Masukkan alamat rumah Anda: ")
+        nama_lengkap = get_input("Masukkan nama lengkap Anda: ")
+        alamat_rumah = get_input("Masukkan alamat rumah Anda: ")
         
-        no_hp = input("Masukkan nomor HP Anda:")
+        no_hp = get_input("Masukkan nomor HP Anda:")
         if not no_hp.isdigit():  
             raise ValueError("Nomor HP harus berupa angka.")
 
@@ -1041,23 +1050,23 @@ def menu_masyarakat(masyarakat):
         print("\033[0m\033[91m4.\033[0m Informasi Akun")
         print("\033[0m\033[91m5.\033[0m Keluar")
 
-        pilihan = input("Silahkan Pilih Menu(1/2/3/4/5): ")
+        pilihan = get_input("Silahkan Pilih Menu(1/2/3/4/5): ")
 
         if pilihan == "1":
             masyarakat.lihat_data_kerusakan()  
-            choice = input("\nIngin Sorting atau Searching Data Kerusakan? (Sort/Search): ").lower()
+            choice = get_input("\nIngin Sorting atau Searching Data Kerusakan? (Sort/Search): ").lower()
             if choice == "sort":
                 masyarakat.menu_sorting_data_kerusakan()  
             elif choice == "search":
-                sub_choice = input("Ingin mencari berdasarkan ID atau Lokasi? (ID/Lokasi): ").lower()
+                sub_choice = get_input("Ingin mencari berdasarkan ID atau Lokasi? (ID/Lokasi): ").lower()
                 if sub_choice == "id":
-                    id_data = input("Masukkan ID data yang ingin dicari: ")
+                    id_data = get_input("Masukkan ID data yang ingin dicari: ")
                     if id_data.isdigit():  
                         masyarakat.menu_search_data_kerusakan("ID_data", id_data)  
                     else:
                         print("ID harus berupa angka. Silakan coba lagi.")
                 elif sub_choice == "lokasi":
-                    lokasi = input("Masukkan nama lokasi yang ingin dicari: ")
+                    lokasi = get_input("Masukkan nama lokasi yang ingin dicari: ")
                     masyarakat.menu_search_data_kerusakan("lokasi", lokasi)  
                 else:
                     print("Pilihan tidak valid. Silakan coba lagi.")
@@ -1068,7 +1077,7 @@ def menu_masyarakat(masyarakat):
             print("\033[0m\033[91m1.\033[0m Diagram batang")
             print("\033[0m\033[91m2.\033[0m Diagram Pie")
             print("\033[0m\033[91m3.\033[0m Diagram Garis")
-            pilihan = input("Silahkan Pilih Diagram (1/2/3): ")
+            pilihan = get_input("Silahkan Pilih Diagram (1/2/3): ")
             if pilihan == "1":
                 masyarakat.lihat_diagram_kerusakan_bar()  
             elif pilihan == "2":
@@ -1078,8 +1087,8 @@ def menu_masyarakat(masyarakat):
             else:
                 print("Pilihan tidak ada!")
         elif pilihan == "3":
-            lokasi = input("Masukkan lokasi kerusakan: ")
-            keterangan_aduan = input("Masukkan deskripsi aduan: ")
+            lokasi = get_input("Masukkan lokasi kerusakan: ")
+            keterangan_aduan = get_input("Masukkan deskripsi aduan: ")
             if not lokasi or not keterangan_aduan:   
                 print("Lokasi dan deskripsi aduan harus diisi!. Gagal membuat aduan.")
             else:
@@ -1163,12 +1172,12 @@ def main():
     print("\033[0m\033[91m2.\033[0m Registrasi")
 
     while True:
-        pilihan = input("Masukkan pilihan Anda (1/2): ")
+        pilihan = get_input("Masukkan pilihan Anda (1/2): ")
         if pilihan.lower() == "1":
             print("")
             print("==================LOGIN=================")
-            username = input("Masukkan Username Anda: ")
-            password = input("Masukkan Password/NIK Anda: ")
+            username = get_input("Masukkan Username Anda: ")
+            password = get_input("Masukkan Password/NIK Anda: ")
 
             user_type = cek_login(username, password)  
             if user_type == "admin":  
